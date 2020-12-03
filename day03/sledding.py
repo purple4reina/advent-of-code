6dfb17f1e148cc4707f822b76047c7aa4eaba533
Rey Abolofia <rey.abolofia@stitchfix.com>
2020-12-03 07:09:58 -0800

    Complete day 3 part 2.

--- day03/sledding.py
+++ day03/sledding.py
@@ -1,14 +1,27 @@
 def main(f):
+    total = 1
+    total *= traverse(1, 1, f)
+    total *= traverse(3, 1, f)
+    total *= traverse(5, 1, f)
+    total *= traverse(7, 1, f)
+    total *= traverse(1, 2, f)
+    return total
+
+
+def traverse(right, down, f):
     i = 0
     trees = 0
+    d = 0
     for row in f:
         row = row.strip()
-        if row[i] == '#':
+        if row[i] == '#' and not d:
             trees += 1
-        i = (i + 3) % len(row)
+        if not d:
+            i = (i + right) % len(row)
+        d = (d + 1) % down
     return trees
 
 
 if __name__ == '__main__':
     with open('input.txt') as f:
-        print(main(f))
+        print(main(list(f)))

eba239ebb2a59051bb5c27593ff418e7d6276692
Rey Abolofia <rey.abolofia@stitchfix.com>
2020-12-07 16:06:50 -0800

    Complete day 7 part 2.

--- day07/luggage.go
+++ day07/luggage.go
@@ -4,18 +4,34 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
 	"strings"
 )
 
 type bag struct {
-	name    string
-	parents []*bag
+	name     string
+	parents  []*bag
+	children []child
 }
 
-func (b *bag) countParents() (parents int) {
+type child struct {
+	*bag
+	count int
+}
+
+func (b *bag) countParents() int {
 	found := make(map[string]struct{})
 	search(b, found)
 	return len(found)
+}
+
+func (b *bag) countChildren() (children int) {
+	for _, c := range b.children {
+		if c.count > 0 {
+			children += c.count + c.count*c.countChildren()
+		}
+	}
+	return
 }
 
 func search(b *bag, found map[string]struct{}) {
@@ -45,7 +61,34 @@ func main() {
 	}
 
 	answer := getParents(string(data))
-	fmt.Println(answer)
+	fmt.Println("parents:", answer)
+	answer = getChildren(string(data))
+	fmt.Println("children:", answer)
+}
+
+func getChildren(data string) int {
+	bm := make(bagManager)
+
+	for _, b := range strings.Split(data, "\n") {
+		pc := strings.SplitN(b, "bags contain", 2)
+
+		parent := strings.Trim(pc[0], " ")
+		pb := bm.getBag(parent)
+
+		for _, c := range strings.Split(strings.Trim(pc[1], " "), ", ") {
+			c = strings.Trim(c, ".")
+			c = strings.Trim(c, "s")
+			c = strings.Replace(c, " bag", "", 1)
+			count, _ := strconv.Atoi(strings.SplitN(c, " ", 2)[0])
+			cname := strings.SplitN(c, " ", 2)[1]
+
+			cb := bm.getBag(cname)
+
+			pb.children = append(pb.children, child{bag: cb, count: count})
+		}
+	}
+
+	return bm.getBag("shiny gold").countChildren()
 }
 
 func getParents(data string) int {

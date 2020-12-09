76a91a73a89500f43684754ee2844f982dc40836
Rey Abolofia <rey.abolofia@stitchfix.com>
2020-12-09 07:46:41 -0800

    Complete day 8 part 2.

--- day08/loop.py
+++ day08/loop.py
@@ -9,13 +9,31 @@ def parse_machine(f):
 
 def solve(f):
     machine = parse_machine(f)
-    states = {}
 
+    for i in range(len(machine)):
+        new_machine = machine.copy()
+        if new_machine[i][0] == 'jmp':
+            new_machine[i] = ('nop', new_machine[i][1])
+        elif new_machine[i][0] == 'nop':
+            new_machine[i] = ('jmp', new_machine[i][1])
+        acc, looped = run(new_machine)
+        if not looped:
+            break
+
+    return acc
+
+
+def run(machine):
     accumulator = 0
     index = 0
+    states = {}
+    looped = False
     while True:
         states[index] = True
-        cmd, amt = machine[index]
+        try:
+            cmd, amt = machine[index]
+        except IndexError:
+            break
         if cmd == 'acc':
             accumulator += amt
             index += 1
@@ -24,9 +42,10 @@ def solve(f):
         else:
             index += 1
         if states.get(index):
+            looped = True
             break
 
-    return accumulator
+    return accumulator, looped
 
 
 if __name__ == '__main__':

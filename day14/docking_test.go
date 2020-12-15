6c6c5c0e87a84071e91ff5e7ed2798c95a114d4c
Rey Abolofia <rey.abolofia@stitchfix.com>
2020-12-15 07:52:16 -0800

    Add tests for updating a single bit.

--- day14/docking_test.go
+++ day14/docking_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -13,5 +14,75 @@ mem[26] = 1`, "\n")
 	answer := Solve(input)
 	if answer != 208 {
 		t.Errorf("incorrect answer: expect=208, actual=%d", answer)
+	}
+}
+
+func TestUpdateBit(t *testing.T) {
+	testcases := []struct {
+		input  string
+		place  int64
+		digit  int64
+		expect string
+	}{
+		{
+			input:  "111",
+			place:  0,
+			digit:  0,
+			expect: "110",
+		},
+		{
+			input:  "111",
+			place:  1,
+			digit:  0,
+			expect: "101",
+		},
+		{
+			input:  "111",
+			place:  2,
+			digit:  0,
+			expect: "11",
+		},
+		{
+			input:  "111",
+			place:  3,
+			digit:  0,
+			expect: "111",
+		},
+		{
+			input:  "111",
+			place:  3,
+			digit:  1,
+			expect: "1111",
+		},
+		{
+			input:  "100",
+			place:  0,
+			digit:  1,
+			expect: "101",
+		},
+		{
+			input:  "111",
+			place:  0,
+			digit:  1,
+			expect: "111",
+		},
+		{
+			input:  "100",
+			place:  0,
+			digit:  0,
+			expect: "100",
+		},
+	}
+
+	for _, test := range testcases {
+		i, err := strconv.ParseInt(test.input, 2, 64)
+		if err != nil {
+			t.Fatal(err)
+		}
+		updated := updateBit(i, test.place, test.digit)
+		actual := strconv.FormatInt(updated, 2)
+		if actual != test.expect {
+			t.Errorf("incorrect bits flipped: expected=%s actual=%s", test.expect, actual)
+		}
 	}
 }

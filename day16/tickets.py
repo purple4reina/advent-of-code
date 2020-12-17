323dc69086d5707df709f818770afe9f736b0b9b
Rey Abolofia <rey.abolofia@stitchfix.com>
2020-12-16 21:16:50 -0800

    Complete day 16 part 2.

--- day16/tickets.py
+++ day16/tickets.py
@@ -1,7 +1,7 @@
 def parse(inp):
     section = 0
     skip = False
-    ranges = {}
+    ranges = {'all': {}}
     tickets = []
     for line in inp:
         if skip:
@@ -13,26 +13,54 @@ def parse(inp):
             skip = True
             continue
         if section == 0:
-            _, nums = line.split(': ')
+            field, nums = line.split(': ')
+            ranges[field] = {}
             for r in nums.split(' or '):
                 bottom, top = map(int, r.split('-'))
                 for n in range(bottom, top + 1):
-                    ranges[n] = True
+                    ranges[field][n] = True
+                    ranges['all'][n] = True
         elif section == 1:
-            pass
+            myticket = tuple(map(int, line.split(',')))
         else:
-            tickets.append(map(int, line.split(',')))
-    return ranges, tickets
+            tickets.append(tuple(map(int, line.split(','))))
+    return ranges, myticket, tickets
 
 
 def solve(inp):
-    ranges, tickets = parse(inp)
-    total = 0
+    ranges, myticket, tickets = parse(inp)
+    possibles = {i: {} for i in range(len(tickets[0]))}
     for ticket in tickets:
-        for val in ticket:
-            if not ranges.get(val):
-                total += val
-    return total
+        for i, val in enumerate(ticket):
+            if not ranges['all'].get(val):
+                break
+            for field, rngs in ranges.items():
+                if not rngs.get(val):
+                    possibles[i][field] = False
+
+    del ranges['all']
+    fields = len(ranges)
+    key = {}
+
+    while possibles:
+        for i, pos in list(possibles.items()):
+            if len(pos) == fields - 1:
+                for field in ranges:
+                    if field not in pos:
+                        key[field] = i
+                        del possibles[i]
+                        del ranges[field]
+                        fields -= 1
+                        for p in possibles.values():
+                            if field in p:
+                                del p[field]
+                        break
+
+    prod = 1
+    for k, v in key.items():
+        if k.startswith('departure'):
+            prod *= myticket[v]
+    return prod
 
 
 if __name__ == '__main__':

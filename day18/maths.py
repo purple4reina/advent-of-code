d16487e4d285fe068c9bef7616c08fed121daac0
Rey Abolofia <rey.abolofia@stitchfix.com>
2020-12-19 13:38:01 -0800

    Complete day 18 part 1.

---
+++ day18/maths.py
@@ -0,0 +1,44 @@
+def add(a, b):
+    return a + b
+
+
+def mult(a, b):
+    return a * b
+
+
+def maths(exp):
+    total, parens, parens_i, oper = 0, 0, None, add
+    for i, char in enumerate(exp):
+        if char == " ":
+            continue
+        elif char == "+":
+            if parens == 0:
+                oper = add
+        elif char == "*":
+            if parens == 0:
+                oper = mult
+        elif char == "(":
+            parens += 1
+            if parens_i is None:
+                parens_i = i
+        elif char == ")":
+            parens -= 1
+            if parens == 0:
+                total = oper(total, maths(exp[parens_i+1:i]))
+                parens_i = None
+        else:
+            if parens == 0:
+                total = oper(total, int(char))
+    return total
+
+
+def solve(f):
+    total = 0
+    for line in f:
+        total += maths(line)
+    return total
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        print(solve([l.strip() for l in f]))

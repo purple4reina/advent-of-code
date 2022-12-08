4e04d8167e9f4428ac40f74876dbbb1756039bff
Rey Abolofia <purple4reina@gmail.com>
2022-12-07 22:31:42 -0800

    Initial.

---
+++ 2022/day08/day_test.py
@@ -0,0 +1,50 @@
+import pytest
+
+from day import part1, part2, process
+
+_test_input = """
+30373
+25512
+65332
+33549
+35390
+""".strip()
+_test_part1_expect = 21
+_test_part2_expect = None
+
+_test_part1 = (
+        (_test_input, _test_part1_expect),
+)
+
+@pytest.mark.parametrize('raw,expect', _test_part1)
+def test_part1(raw, expect):
+    inputs = process(raw)
+    assert expect == part1(inputs)
+
+_test_part2 = (
+        (_test_input, _test_part2_expect),
+)
+
+@pytest.mark.parametrize('raw,expect', _test_part2)
+def test_part2(raw, expect):
+    inputs = process(raw)
+    assert expect == part2(inputs)
+
+with open('input.txt') as f:
+    _actual_inputs = f.read()
+
+_part1_wrong_answers = (
+)
+
+@pytest.mark.parametrize('ans', _part1_wrong_answers)
+def test_part1_wrong_answers(ans):
+    inputs = process(_actual_inputs)
+    assert ans != part1(inputs)
+
+_part2_wrong_answers = (
+)
+
+@pytest.mark.parametrize('ans', _part2_wrong_answers)
+def test_part2_wrong_answers(ans):
+    inputs = process(_actual_inputs)
+    assert ans != part2(inputs)

7dbafeea0df28a766013f3276499691cc1ccbb30
Rey Abolofia <purple4reina@gmail.com>
2022-12-08 20:44:15 -0800

    Blank slate.

---
+++ 2022/day09/day_test.py
@@ -0,0 +1,45 @@
+import pytest
+
+from day import part1, part2, process
+
+_test_input = """
+""".strip()
+_test_part1_expect = None
+_test_part2_expect = None
+
+with open('input.txt') as f:
+    _actual_inputs = f.read()
+
+_test_part1 = (
+        (_test_input, _test_part1_expect),
+)
+
+@pytest.mark.parametrize('raw,expect', _test_part1)
+def test_part1(raw, expect):
+    inputs = process(raw)
+    assert expect == part1(inputs)
+
+_test_part2 = (
+        (_test_input, _test_part2_expect),
+)
+
+@pytest.mark.parametrize('raw,expect', _test_part2)
+def test_part2(raw, expect):
+    inputs = process(raw)
+    assert expect == part2(inputs)
+
+_part1_wrong_answers = (
+)
+
+@pytest.mark.parametrize('ans', _part1_wrong_answers)
+def test_part1_wrong_answers(ans):
+    inputs = process(_actual_inputs)
+    assert ans != part1(inputs)
+
+_part2_wrong_answers = (
+)
+
+@pytest.mark.parametrize('ans', _part2_wrong_answers)
+def test_part2_wrong_answers(ans):
+    inputs = process(_actual_inputs)
+    assert ans != part2(inputs)

cef9c3d97d0b43e8d0fa3b397201830c01a5e787
Rey Abolofia <purple4reina@gmail.com>
2022-12-13 21:40:02 -0800

    Part 1.

--- 2022/day14/day.py
+++ 2022/day14/day.py
@@ -1,5 +1,39 @@
-def part1(inputs):
-    pass
+def part1(rocks):
+    world = {}
+    for path in rocks:
+        endx, endy = path.pop(0)
+        while path:
+            (startx, starty), (endx, endy) = (endx, endy), path.pop(0)
+            stepx = 2*(endx > startx) - 1
+            stepy = 2*(endy > starty) - 1
+            for x in range(startx, endx+stepx, stepx):
+                for y in range(starty, endy+stepy, stepy):
+                    world[(x, y)] = True
+
+    rockx, rocky, count = 500, 0, 0
+    while True:
+        # if in infinite pit, break
+        if rocky > 999:
+            return count
+
+        # fall down
+        elif (rockx, rocky + 1) not in world:
+            rockx, rocky = rockx, rocky + 1
+
+        # fall diagonal left
+        elif (rockx - 1, rocky + 1) not in world:
+            rockx, rocky = rockx - 1, rocky + 1
+
+        # fall diagonal right
+        elif (rockx + 1, rocky + 1) not in world:
+            rockx, rocky = rockx + 1, rocky + 1
+
+        # else stop, next rock
+        else:
+            world[(rockx, rocky)] = True
+            rockx, rocky, count = 500, 0, count + 1
+
+    return count
 
 def part2(inputs):
     pass
@@ -11,7 +45,15 @@ def read_inputs():
         return f.read().strip()
 
 def process(raw):
-    return list(map(int, raw.split()))
+    rocks = []
+    for path in raw.split('\n'):
+        if not path:
+            break
+        rock = []
+        for loc in path.split(' -> '):
+            rock.append(eval(loc))
+        rocks.append(rock)
+    return rocks
 
 if __name__ == '__main__':
     read = read_inputs()

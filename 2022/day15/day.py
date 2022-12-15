0329181129e8a9a3d7e29b14c32adb29f489dca0
Rey Abolofia <purple4reina@gmail.com>
2022-12-14 22:23:49 -0800

    Add code back in.

--- 2022/day15/day.py
+++ 2022/day15/day.py
@@ -34,37 +34,37 @@ def part2(points, most=20):
     for sx, sy, bx, by in points:
         dist = abs(sx - bx) + abs(sy - by)
 
-#        if sx < 0:
-#            if sx + dist < 0:
-#                continue
-#        elif sx > most:
-#            if sx - dist > most:
-#                continue
-#        elif sy < 0:
-#            if sy + dist < 0:
-#                continue
-#        elif sy > most:
-#            if sy - dist > most:
-#                continue
+        if sx < 0:
+            if sx + dist < 0:
+                continue
+        elif sx > most:
+            if sx - dist > most:
+                continue
+        elif sy < 0:
+            if sy + dist < 0:
+                continue
+        elif sy > most:
+            if sy - dist > most:
+                continue
 
         for y in range(dist + 1):
-            #            if sy + y < 0 and sy + y > most and sy - y < 0 and sy - y > most:
-            #                continue
+            if sy + y < 0 and sy + y > most and sy - y < 0 and sy - y > most:
+                continue
             for x in range(dist - y + 1):
-                #                if sx + x > most:
-                #                    break
+                #if sx + x > most:
+                #    break
                 world[(sx + x, sy + y)] = True
                 world[(sx + x, sy - y)] = True
                 world[(sx - x, sy + y)] = True
                 world[(sx - x, sy - y)] = True
 
-    #import numpy as np
-    #w = np.array([[x, y] for x, y in world.keys()])
-    #import matplotlib.pyplot as plot
-    #plot.plot(w[:,0], w[:,1], 'ro')
-    #plot.xlim([0, most])
-    #plot.ylim([0, most])
-    #plot.show()
+    import numpy as np
+    w = np.array([[x, y] for x, y in world.keys()])
+    import matplotlib.pyplot as plot
+    plot.plot(w[:,0], w[:,1], 'ro')
+    plot.xlim([0, most])
+    plot.ylim([0, most])
+    plot.show()
 
     xs = [0] * (most + 1)
     ys = [0] * (most + 1)
@@ -73,7 +73,6 @@ def part2(points, most=20):
             xs[x] += 1
             ys[y] += 1
 
-    print('xs,ys: ', xs,ys)
     lx = ly = float('inf')
     ix = iy = None
     for i, x in enumerate(xs):

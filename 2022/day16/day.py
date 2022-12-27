06e8ea727eed0257af7a584ce9734e93424dd874
Rey Abolofia <purple4reina@gmail.com>
2022-12-26 17:31:21 -0800

    Breadth first search.

--- 2022/day16/day.py
+++ 2022/day16/day.py
@@ -1,5 +1,53 @@
-def part1(inputs):
-    pass
+def part1(system):
+
+    states, new_states = [(0, 'AA', [])], []
+    top = 0
+    for minute in range(1, 31):
+        for total, loc, opened in states:
+
+            rate, valves = system[loc]
+
+            # open
+            if loc not in opened:
+                new_states.append((total + rate*(30-minute), loc, opened+[loc]))
+
+            # move
+            for valve in valves:
+                new_states.append((total, valve, opened))
+
+        states, new_states = new_states, []
+        states.sort(reverse=True)
+        states = states[:len(states)//2+4]
+
+    top = 0
+    for total, _, _ in states:
+        top = max(top, total)
+    return top
+
+#    most = [0]
+#    def release(loc, minute, opened):
+#        minute += 1
+#
+#        if minute > 30:
+#            return 0
+#
+#        rate, valves = system[loc]
+#
+#        # open valve
+#        val = 0
+#        if loc not in opened:
+#            val = max(rate*(30-minute) + release(loc, minute, opened+[loc]), val)
+#
+#        # move
+#        for valve in valves:
+#            val = max(release(valve, minute, opened), val)
+#
+#        if val > most[0]:
+#            most[0] = val
+#            print('val: ', val)
+#        return val
+#
+#    return release('AA', 0, [])
 
 def part2(inputs):
     pass
@@ -11,7 +59,17 @@ def read_inputs():
         return f.read().strip()
 
 def process(raw):
-    return list(map(int, raw.split()))
+    import re
+    system = {}
+    row_re = re.compile(
+            r'Valve (..) has flow rate=(\d*); tunnels? leads? to valves? (.*)')
+    for row in raw.split('\n'):
+        m = row_re.match(row)
+        loc = m.group(1)
+        rate = int(m.group(2))
+        valves = list(m.group(3).split(', '))
+        system[loc] = (rate, valves)
+    return system
 
 if __name__ == '__main__':
     read = read_inputs()

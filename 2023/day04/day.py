5f3f81381333f448e13a5b07156c7812be5e6f37
Rey Abolofia <purple4reina@gmail.com>
2023-12-04 16:40:02 -0800

    Day 4.

---
+++ 2023/day04/day.py
@@ -0,0 +1,19 @@
+def part1(inputs):
+    pass
+
+def part2(inputs):
+    pass
+
+def read_inputs():
+    import os
+    file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'input.txt')
+    with open(file) as f:
+        return f.read().strip()
+
+def process(raw):
+    return list(raw.split())
+
+if __name__ == '__main__':
+    read = read_inputs()
+    print(part1(process(read)))
+    print(part2(process(read)))

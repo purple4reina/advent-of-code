a949451e6d3cc456734c11446c79f26120054123
Rey Abolofia <purple4reina@gmail.com>
2023-12-14 17:09:04 -0800

    Day 11, part 2, but it crashes.

--- 2023/day11/day.py
+++ 2023/day11/day.py
@@ -27,8 +27,32 @@ def part1(universe):
 
     return dist
 
-def part2(inputs):
-    pass
+def part2(universe):
+    rows, columns = universe.shape
+    expanded = np.array(universe)
+    for row in reversed(range(rows)):
+        if not np.sum(universe[row]):
+            expanded = np.insert(expanded, row, np.zeros((1000000, 1)), axis=0)
+    for column in reversed(range(columns)):
+        if not np.sum(universe[:,column]):
+            expanded = np.insert(expanded, column, np.zeros((1000000, 1)), axis=1)
+
+    points = []
+    exrows, excolumns = expanded.shape
+    for y in range(exrows):
+        for x in range(excolumns):
+            if expanded[y,x] == 1:
+                points.append((x, y))
+
+    dist = 0
+    pntcnt = len(points)
+    for i in range(pntcnt):
+        point1 = points[i]
+        for j in range(i + 1, pntcnt):
+            point2 = points[j]
+            dist += abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
+
+    return dist
 
 def read_inputs():
     with open('input.txt') as f:
